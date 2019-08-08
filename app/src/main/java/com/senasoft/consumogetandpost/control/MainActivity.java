937174352a6579b8eb1ffd43222b30403d6ffeee
CristhianCanar <criacamu@gmail.com>
2019-08-08 09:21:14 -0500

    Se hacen cambios getSer

--- app/src/main/java/com/senasoft/consumogetandpost/control/MainActivity.java
+++ app/src/main/java/com/senasoft/consumogetandpost/control/MainActivity.java
@@ -1,8 +1,10 @@
-package com.senasoft.consumogetandpost;
+package com.senasoft.consumogetandpost.control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.senasoft.consumogetandpost.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/senasoft/consumogetandpost/services/GetServices.java
@@ -0,0 +1,58 @@
+package com.senasoft.consumogetandpost.services;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class GetServices extends AsyncTask<String,Void,String> {
+    OkHttpClient client =  new OkHttpClient();
+
+
+    public interface AsyncResponse{
+        void processFinish (String output);
+    }
+
+    public GetServices.AsyncResponse delegate = null;
+
+    public GetServices(AsyncResponse delegate) {
+        this.delegate = delegate;
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        RequestBody body = new FormBody.Builder()
+                .add("name_product",params[1])
+                .add("volume_product",params[2])
+                .add("value_product",params[3])
+                .add("stock_product",params[4])
+                .add("url_image_product",params[5])
+                .build();
+
+        Request request = new Request.Builder().url(params [0]).post(body).build();
+            try {
+                Response response = client.newCall(request).execute();
+
+                if (!response.isSuccessful())
+                    throw new IOException("LOGG"+response.toString());
+
+                return response.body().string();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+    }
+}
